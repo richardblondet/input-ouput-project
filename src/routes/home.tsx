4f786824af162ff066f609cbd01d81ebd2b1dc55
richardblondet <richardblondet@gmail.com>
2020-11-30 14:53:28 -0400

    home page components

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,6 +4,7 @@ import { Box, Container, Row, Col, Navbar, Button } from '../components/Utils';
 import Logo from '../components/Logo';
 import { FiEdit2 } from 'react-icons/fi';
 import { useContent, useIntl } from '../store/hooks';
+import DateRenderer from '../components/DateRenderer';
 
 const { Fragment } = React;
 const { H1, Paragraph, Span, Link } = Typography;
@@ -23,26 +24,26 @@ const Home = () => {
       {/* Hero Section */}
       <Box py="6rem" backgroundColor="primary-tints.100">
         <Container>
-          <Row>
-            <Col sm={6}>
-              <H1 fontWeight="900">Welcome to the <br />
-                <Span color="primary"> Input/Outout</Span> Project</H1>
+          <Box display="flex" minHeight="25rem" style={{ alignItems: 'center', flexWrap: "wrap" }}>
+            
+            <Box width={[ 1, 1/2 ]} alignSelf="center">
+              <H1 fontWeight="900" color="primary-shades.900">
+                Welcome to the <br /><Span color="primary"> Input/Outout</Span> Project
+              </H1>
                 
               <Paragraph color="secondary">
                 {editor[locale]}
               </Paragraph>
-
-              <Link to="/dashboard" as="span">
-                <Button fontWeight={700} py="m" px="l" type="primary">
-                  <Box mr=".375rem" display="inline" position="relative" top={3}><FiEdit2 /></Box>
-                  Edit this Page
-                </Button>
-              </Link>
-            </Col>
-            <Col sm={6}>
-              <code>{date.toISOString().split('T')[0]}</code>
-            </Col>
-          </Row>
+            
+              <Button fontWeight={700} py="m" px="l" type="primary">
+                <Box mr=".375rem" display="inline" position="relative" top={3}><FiEdit2 /></Box>
+                Edit this Page
+              </Button>
+            </Box>
+            <Box width={[ 1, 1/2 ]} display="flex" style={{ alignSelf: 'center', flexWrap: "wrap" }}>
+              <DateRenderer date={date} />
+            </Box>
+          </Box>
         </Container>
       </Box>
     </Fragment>
